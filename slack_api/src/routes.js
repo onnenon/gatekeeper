b0fd70dc9c84427ce3e41862cf769e83a97f03ed
michaelvanbosch <54322074+michaelvanbosch@users.noreply.github.com>
2019-09-30 19:50:24 -0500

    Add comment describing endpoint functions

--- slack_api/src/routes.js
+++ slack_api/src/routes.js
@@ -10,15 +10,15 @@ let handleRoutes = function(app) {
         res.send(200);
     });
 
-    app.post('/events', (req, res) => {
+    app.post('/events', (req, res) => { //Parse '/' commands from slack
         commandsController.run(req, res);
     });
 
-    app.post('/interactions', async (req, res) => {
+    app.post('/interactions', async (req, res) => { //Handles callback functions when someone submits a modal
         interactionsController.run(req, res);
     });
 
-    app.post('/data', async (req, res) => {
+    app.post('/data', async (req, res) => { //Used for custom slack data for filling the teams in the modal dropdowns
         let teams = await teamService.getTeams();
         res.status(200).send(JSON.stringify(teams));
     });
